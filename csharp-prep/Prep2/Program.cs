93d2c35a0068df3a2780c58020a8b6ed761b7f26
Carlos Leon <35080984+chinchay@users.noreply.github.com>
2023-01-06 23:25:17 -0300

    feat: Complete task 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What was the grade percentage?");
+        int grade = int.Parse( Console.ReadLine() );
+
+        string letter;
+        if (grade >= 90){
+            // Console.WriteLine("A");
+            letter = "A";
+        }
+        else if (grade >= 80){
+            // Console.WriteLine("B");
+            letter = "B";
+        }
+        else if (grade >= 70){
+            // Console.WriteLine("C");
+            letter = "C";
+        }
+        else if (grade >= 60){
+            // Console.WriteLine("D");
+            letter = "D";
+        }
+        else{
+            // Console.WriteLine("F");
+            letter = "F";
+        }
+        
+        int remainder = grade % 10;
+        string sign = "";
+        if (remainder < 3){
+            if (letter != "F"){
+                sign = "-";
+            }
+        } else if (remainder > 7){
+            if ( (letter != "A") && (letter != "F") ){
+                sign = "+";
+            }
+        }
+
+        Console.WriteLine(letter + sign);
+
+
+
+        if (grade > 70){
+            Console.WriteLine("user passed the course. Congratulations!");
+
+        }
+        else{
+            Console.WriteLine("Next time will be better!");
+        }
+
     }
 }
