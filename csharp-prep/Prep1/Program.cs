c5afe6aaaefe64b24d528d4172a15e38c50c838f
Carlos Leon <35080984+chinchay@users.noreply.github.com>
2023-01-06 23:02:03 -0300

    feat: Complete task 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        string longName = lastName + ", " + firstName + " " + lastName;
+        Console.WriteLine("Your name is " + longName + ".");
+
+        // Alternative:
+        // Console.WriteLine($"Your name is {last}, {first} {last}.");
+
     }
 }
