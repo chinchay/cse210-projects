4a1fc7944ff7ef63901ae178a22ba3d72a42fb4d
Carlos Leon <35080984+chinchay@users.noreply.github.com>
2023-01-09 18:15:33 -0300

    feat: Guess and play

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        int iMax  = 10;
+        int i     = 0;
+        int guess = 0;
+        string want2play = "yes";
+        do{
+            i = 0;
+            Console.WriteLine("What is the magic number?");
+            // int magic = int.Parse( Console.ReadLine() );
+            Random randomGenerator = new Random();
+            int magic = randomGenerator.Next(1, 101);
+
+            do {
+                Console.WriteLine("What is your guess?");
+                guess = int.Parse( Console.ReadLine() );
+
+                if (magic < guess) {
+                    Console.WriteLine("Lower");
+                } else if (magic > guess) {
+                    Console.WriteLine("Higher");
+                } else {
+                    Console.WriteLine("You guessed it!");
+                }
+                i += 1;
+            } while ( (magic != guess) && (i < iMax) );
+
+            Console.WriteLine($"You tried {i} attempts!");
+            Console.WriteLine("Do you want to play again?");
+            want2play = Console.ReadLine();
+        } while (want2play == "yes");
+
+
     }
 }
