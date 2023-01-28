04d8d3b357c2aa222d9b572a842d41c8823e2a8f
Carlos Leon <35080984+chinchay@users.noreply.github.com>
2023-01-28 20:57:19 -0300

    docs: Explain format of json file

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -20,6 +20,29 @@ class Program
         //    `_fileHasBeenLoaded` to True and so it can overwrite all content
         // - Avoid closing the program if entries are not saved, by turning
         //    `_areEntriesSaved` to True
+        //
+        // Included with this code is a file called sample.json
+        //
+        // [
+        // 
+        //  {
+        //      "date":"2022",
+        //      "question":"how are you?",
+        //      "answer":"I am fine"
+        //  },
+        // 
+        //  {
+        //      "date":"2023",
+        //      "question":"how are you??",
+        //      "answer":"I am fine!"
+        //   }
+        // ]
+        //
+        // Or you can use your own file with the following format, as a list
+        // of dictionaries:
+        //
+        //  [ {"date":"2023-01-01", "question": "a question", "answer", "myAnswer"} ]
+        //
         //*********************************************************************
 
 
